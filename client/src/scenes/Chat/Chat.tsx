fe8d181ee1a91310341816af88b91a286edeff6c
RoshanDasan <edroshan2001@gmail.com>
2023-07-01 12:59:37 +0530

    final chat test

--- client/src/scenes/Chat/Chat.tsx
+++ client/src/scenes/Chat/Chat.tsx
@@ -29,7 +29,7 @@ const Chat = () => {
   };
 
   useEffect(() => {
-    socket.current = io('https://connectfy.online/api');
+    socket.current = io('wss://connectfy.online/api');
     if (userId) {
       socket.current.emit('new-user-add', userId);
   
